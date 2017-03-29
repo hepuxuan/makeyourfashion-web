5d5b8e0d440d2e2c88ec865a5c143a4e4a2cbd12
Puxuan He <phe@expedia.com>
2017-03-29 17:01:45 -0500

    add flow

--- src/components/create-shirt/index.js
+++ src/components/create-shirt/index.js
@@ -1,64 +1,77 @@
-import React from 'react'
-import CreateShirtCanvas from './CreateShirtCanvas'
-import css from './create-shirt.css'
-import {Grid, Cell, Button, Spinner} from 'react-mdl'
-import LeftPanel from './LeftPanel'
-import SelectProduct from './SelectProduct'
-import SelectDesign from './SelectDesign'
-import {connect} from 'react-redux'
-import OrderForm from './OrderForm'
-import { fetchProducts, addToCart } from '../../action'
+// @flow
 
+import React from 'react';
+import { connect } from 'react-redux';
+import { Grid, Cell, Button, Spinner } from 'react-mdl';
+import CreateShirtCanvas from './CreateShirtCanvas';
+import css from './create-shirt.css';
+import LeftPanel from './LeftPanel';
+import SelectProduct from './SelectProduct';
+import SelectDesign from './SelectDesign';
+import OrderForm from './OrderForm';
+import { fetchProducts, addToCart } from '../../action';
+
+// $FlowFixMe decorators
 @connect(state => ({
   products: state.entities.products,
-  order: state.ui.createOrder.order
+  order: state.ui.createOrder.order,
 }), dispatch => ({
-  fetchProducts () {
-    dispatch(fetchProducts())
+  fetchProducts() {
+    dispatch(fetchProducts());
   },
-  addToCart (order) {
-    dispatch(addToCart(order))
-  }
+  addToCart(order) {
+    dispatch(addToCart(order));
+  },
 }))
 export default class CreateShirt extends React.Component {
   componentDidMount() {
-    this.props.fetchProducts()
+    this.props.fetchProducts();
   }
 
-  handleAddToCard = e => {
-    e.preventDefault()
+  props: {
+    fetchProducts: () => void,
+    addToCart: (any) => void,
+    order: {
+      productId: number
+    },
+    products: Array<{
+      imgUrl: string
+    }>
+  }
+
+  handleAddToCard = (e: Event) => {
+    e.preventDefault();
     this.props.addToCart({
       ...this.props.order,
       // use the actual image from backend
-      imgUrl: this.props.products[this.props.order.productId].imgUrl
-    })
+      imgUrl: this.props.products[this.props.order.productId].imgUrl,
+    });
   }
 
-  render () {
-    const product = this.props.products[this.props.order.productId]
-    return (() => {
-      if (!product) {
-        return <Spinner />
-      } else {
-        return <div>
-          <Grid className={css.container}>
-            <Cell col={3}>
-              <LeftPanel />
-            </Cell>
-            <Cell col={6}>
-              <CreateShirtCanvas />
-            </Cell>
-            <Cell col={3}>
-              <OrderForm />
-            </Cell>
-          </Grid>
-          <SelectProduct />
-          <SelectDesign />
-          <div className={css.actionarea}>
-            <Button onClick={this.handleAddToCard} className={css.actionbutton} accent ripple raised>添加到购物车</Button>
-          </div>
+  render() {
+    const product = this.props.products[this.props.order.productId];
+    return !product ? <Spinner /> : (
+      <div>
+        <Grid className={css.container}>
+          <Cell col={3}>
+            <LeftPanel />
+          </Cell>
+          <Cell col={6}>
+            <CreateShirtCanvas />
+          </Cell>
+          <Cell col={3}>
+            <OrderForm />
+          </Cell>
+        </Grid>
+        <SelectProduct />
+        <SelectDesign />
+        <div className={css.actionarea}>
+          <Button
+            onClick={this.handleAddToCard}
+            className={css.actionbutton} accent ripple raised
+          >添加到购物车</Button>
         </div>
-      }
-    })()
+      </div>
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { Tabs, Tab, Grid, Cell, Textfield } from 'react-mdl';
+import { Tabs, Tab } from 'react-mdl';
 import reducer from './reducer';
 import CreateShirt from './components/create-shirt';
 import ShoppingCartButton from './components/ShoppingCartButton';
